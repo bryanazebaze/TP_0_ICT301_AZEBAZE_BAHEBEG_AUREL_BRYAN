23990cdf0aa3da024526c58d81325a8ee4612e45
bryanazebaze <187443105+bryanazebaze@users.noreply.github.com>
2025-12-25 19:26:43 +0100

    Réalisation du principe OCP : utilisation d'interfaces pour l'extension

---
+++ OCP/ApresOCP.java
@@ -0,0 +1,61 @@
+package ict301.solid.ocp;
+interface Shape {
+double calculateArea();
+}
+
+class Rectangle implements Shape 
+{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) 
+    {
+    this.width = width;
+    this.height = height;
+    }
+
+    @Override
+    public double calculateArea() 
+    {
+    return width * height;
+    }
+}
+
+class Circle implements Shape 
+{
+    private double radius;
+
+    public Circle(double radius) 
+    {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() 
+    {
+    return Math.PI * radius * radius;
+    }
+}
+
+
+public class AreaCalculator2 
+{
+public double calculateArea(Shape shape) 
+    {
+    return shape.calculateArea();
+    }
+}
+
+
+public class Main 
+{
+
+    public static void main(String[] args)
+    {
+        Shape shape = new Rectangle(4, 3);
+        System.out.println("Area = '" +
+        shape.calculateArea() + "'");
+    }
+}
+
+
